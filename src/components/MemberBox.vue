6ca7175271546ed020c5a18d5ed8e5eabbb29319
Marco Antonio <110636189+DCCXXV@users.noreply.github.com>
2024-12-10 19:41:30 +0100

    Cambios badges modal usuario

--- src/components/MemberBox.vue
+++ src/components/MemberBox.vue
@@ -3,61 +3,73 @@
     de un grupo de posibles referencias
 -->
 <template>
-    <div class="row g-1">
-        <div class="col-3 text-end">
-            <label :for="id" class="form-label">{{ label }}</label>
-        </div>
-        <div class="col-9 text-start">
-            <div v-for="o in possible" :key="o.id" class="caja">
-                <span v-if="current.some(c => c.id == o.id)" class="exists" @click="rm(o.id)">
-                    {{ o.name }}
-                </span>
-                <span v-else @click="current.push(o)">
-                    {{ o.name }}
-                </span>
-            </div>
-            <input type="hidden" :name="id" :id="id" :value="read">
-        </div>
+  <div class="row g-1">
+    <div class="col-3 text-end">
+      <label :for="id" class="form-label">{{ label }}</label>
     </div>
+    <div class="col-9 text-start">
+      <div v-for="o in possible" :key="o.id" class="caja">
+        <span
+          v-if="current.some((c) => c.id == o.id)"
+          class="badge bg-primary exists"
+          @click="rm(o.id)"
+        >
+          {{ o.name }}
+        </span>
+        <span v-else class="badge bg-secondary" @click="current.push(o)">
+          {{ o.name }}
+        </span>
+      </div>
+      <input type="hidden" :name="id" :id="id" :value="read" />
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { gState } from '../state.js'
+import { gState } from "../state.js";
 
-import { ref, computed, onMounted } from 'vue'
+import { ref, computed, onMounted } from "vue";
 
 const props = defineProps({
-    label: String,
-    id: String,
-    start: Array,
-    all: Array,
-})
+  label: String,
+  id: String,
+  start: Array,
+  all: Array,
+});
 
-const current = ref([])
-const possible = ref([])
+const current = ref([]);
+const possible = ref([]);
 
 onMounted(() => {
-    current.value = props.start.map(id => gState.resolve(id));
-    possible.value = props.all.toSorted((a, b) => a.name > b.name);
-})
+  current.value = props.start.map((id) => gState.resolve(id));
+  possible.value = props.all.toSorted((a, b) => a.name > b.name);
+});
 
 function rm(id) {
-    current.value.splice(current.value.findIndex(o => o.id == id), 1);
+  current.value.splice(
+    current.value.findIndex((o) => o.id == id),
+    1
+  );
 }
 
 const read = computed(() => {
-    return JSON.stringify(current.value.map(o => o.id));
-})
-
+  return JSON.stringify(current.value.map((o) => o.id));
+});
 </script>
 
 <style scoped>
 .exists {
-    background-color: lightblue;
+  opacity: 0.8;
+  cursor: pointer;
+}
+
+.badge {
+  cursor: pointer;
+  margin: 2px;
 }
 
 .caja {
-    display: inline-block;
-    padding: 2px;
+  display: inline-block;
+  padding: 2px;
 }
 </style>
